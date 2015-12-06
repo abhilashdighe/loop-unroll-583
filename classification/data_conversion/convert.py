575c570a375415a344f3161eceb0283a9390abd4
= <=>
2015-12-05 21:03:09 -0500

    Added tensorflow NeuralNetwork functions. Need to do model selection

---
+++ classification/data_conversion/convert.py
@@ -0,0 +1,71 @@
+import csv
+import sys
+from sklearn.cross_validation import train_test_split
+import cPickle as pickle
+
+loop_to_features = {}
+loop_to_timings = {}
+with open('features.csv') as features_file:
+    features_csv = csv.reader(features_file)
+    for sample in features_csv:
+        benchmark , loopid = sample[:2]
+        features = map(float , sample[2:])
+        loop_to_features[(benchmark,loopid)] = features
+
+with open('runtime.csv') as runtime_file:
+    runtimes_csv = csv.reader(runtime_file)
+    for sample in runtimes_csv:
+        benchmark , loopid , trip_count , unroll_factor , time = sample
+        # print benchmark , loopid , trip_count , unroll_factor , time
+        time_per_trip = float(time) / float(trip_count)
+        if (benchmark,loopid) in loop_to_timings:
+            if unroll_factor in loop_to_timings[(benchmark,loopid)]:
+                # print loop_to_timings[(benchmark,loopid)]
+                (loop_to_timings[(benchmark,loopid)])[unroll_factor].append(time_per_trip)
+            else:
+                # print loop_to_timings[(benchmark,loopid)]
+                (loop_to_timings[(benchmark,loopid)])[unroll_factor] = [time_per_trip,]
+        else:
+            loop_to_timings[(benchmark,loopid)] = {}
+            loop_to_timings[(benchmark,loopid)][unroll_factor] = [time_per_trip,]
+    
+
+loop_to_label = {}
+for benchmark , loopid in loop_to_timings:
+    
+    best_factor = -1
+    best_time = sys.maxint
+    for unroll_factor in loop_to_timings[(benchmark,loopid)]:
+        curr_timing_list = loop_to_timings[(benchmark,loopid)][unroll_factor]
+        curr_time = sum(curr_timing_list) / len(curr_timing_list)
+        loop_to_timings[(benchmark,loopid)][unroll_factor] = curr_time
+        if curr_time < best_time:
+            best_time = curr_time
+            best_factor = unroll_factor
+
+    loop_to_label[(benchmark,loopid)] = best_factor
+
+
+X = []
+y = []
+with open('labelled_data.csv' , 'wb') as labelled_file:
+    labelled_csv = csv.writer(labelled_file)
+    for benchmark , loopid in loop_to_features:
+        if (benchmark , loopid ) in loop_to_timings:
+            features = loop_to_features[(benchmark,loopid)]
+            X.append(features)
+            label = loop_to_label[(benchmark,loopid)]
+            y.append(int(label))
+            features.append(label)
+            labelled_csv.writerow(features)
+
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.33, random_state=42 , stratify=y)
+
+data = {}
+data['X_train'] = X_train
+data['X_test'] = X_test
+data['y_train'] = y_train
+data['y_test'] = y_test
+
+pickle.dump( data , open('split_data.p' , 'wb'))
