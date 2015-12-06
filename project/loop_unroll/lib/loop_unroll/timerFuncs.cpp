db445d4cd8c5ffcb9767c519f5fa2fec4cfdf849
Pallavi <palmoghe@gmail.com>
2015-12-06 15:47:57 -0500

    Added header to loopTimings.csv

--- project/loop_unroll/lib/loop_unroll/timerFuncs.cpp
+++ project/loop_unroll/lib/loop_unroll/timerFuncs.cpp
@@ -8,20 +8,38 @@
 using namespace std;
 clock_t start;
 ofstream outfile;
-
 map<string, clock_t> loopStartTimes;
+const string OUTFILE = "loopTimings.csv";
+
+inline bool exists(const string& name) {
+    ifstream f(name.c_str());
+    if (f.good()) {
+        f.close();
+        return true;
+    } else {
+        f.close();
+        return false;
+    }
+}
 
 void startTimer(char *loopID) {
+	// Print header in output file
+	if(!exists(OUTFILE)) {
+		outfile.open(OUTFILE, ios_base::app);
+		outfile << "LoopID" <<  ", " << "Trip Count" << ", " << "Duration" << "\n";
+		outfile.close();
+	}
 	start = clock();
 	loopStartTimes[string(loopID)] = start;
 }
 
 void endTimer(char *loopID, int tripCount) {
+
 	double duration;
 	clock_t end = clock();
 	duration = (end - loopStartTimes[string(loopID)]) / (double) CLOCKS_PER_SEC;
 	loopStartTimes.erase(string(loopID));
-	outfile.open("loopTimings.csv", ios_base::app);
+	outfile.open(OUTFILE, ios_base::app);
 	outfile << string(loopID) <<  ", " << tripCount << ", " << duration << "\n";
 	outfile.close();
 }
